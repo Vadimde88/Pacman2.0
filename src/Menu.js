1506f90443e6dd73d26fb0e1768f070c2ba4d3be
Vadimde88 <vadimde88@gmail.com>
2024-12-19 20:27:19 -0500

    added high scores button

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-var Menu = function(title,x,y,w,h,pad,font,fontcolor) {
+var Menu = function(title, x, y, w, h, pad, font, fontcolor) {
     this.title = title;
     this.x = x;
     this.y = y;
@@ -7,16 +7,15 @@ var Menu = function(title,x,y,w,h,pad,font,fontcolor) {
     this.pad = pad;
     this.buttons = [];
     this.buttonCount = 0;
-    this.currentY = this.y+this.pad;
+    this.currentY = this.y + this.pad;
 
     if (title) {
-        this.currentY += 1*(this.h + this.pad);
+        this.currentY += 1 * (this.h + this.pad);
     }
 
     this.font = font;
     this.fontcolor = fontcolor;
     this.enabled = false;
-
     this.backButton = undefined;
 };
 
@@ -24,7 +23,7 @@ Menu.prototype = {
 
     clickCurrentOption: function() {
         var i;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             if (this.buttons[i].isSelected) {
                 this.buttons[i].onclick();
                 break;
@@ -35,10 +34,10 @@ Menu.prototype = {
     selectNextOption: function() {
         var i;
         var nextBtn;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             if (this.buttons[i].isSelected) {
                 this.buttons[i].blur();
-                nextBtn = this.buttons[(i+1)%this.buttonCount];
+                nextBtn = this.buttons[(i + 1) % this.buttonCount];
                 break;
             }
         }
@@ -49,70 +48,72 @@ Menu.prototype = {
     selectPrevOption: function() {
         var i;
         var nextBtn;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             if (this.buttons[i].isSelected) {
                 this.buttons[i].blur();
-                nextBtn = this.buttons[i==0?this.buttonCount-1:i-1];
+                nextBtn = this.buttons[i == 0 ? this.buttonCount - 1 : i - 1];
                 break;
             }
         }
-        nextBtn = nextBtn || this.buttons[this.buttonCount-1];
+        nextBtn = nextBtn || this.buttons[this.buttonCount - 1];
         nextBtn.focus();
     },
 
-    addToggleButton: function(isOn,setOn) {
-        var b = new ToggleButton(this.x+this.pad,this.currentY,this.w-this.pad*2,this.h,isOn,setOn);
+    addToggleButton: function(isOn, setOn) {
+        var b = new ToggleButton(this.x + this.pad, this.currentY, this.w - this.pad * 2, this.h, isOn, setOn);
         this.buttons.push(b);
         this.buttonCount++;
         this.currentY += this.pad + this.h;
     },
 
-    addToggleTextButton: function(label,isOn,setOn) {
-        var b = new ToggleButton(this.x+this.pad,this.currentY,this.w-this.pad*2,this.h,isOn,setOn);
-        b.setFont(this.font,this.fontcolor);
-        b.setToggleLabel(label);
-        this.buttons.push(b);
-        this.buttonCount++;
-        this.currentY += this.pad + this.h;
-    },
-
-    addTextButton: function(msg,onclick) {
-        var b = new Button(this.x+this.pad,this.currentY,this.w-this.pad*2,this.h,onclick);
-        b.setFont(this.font,this.fontcolor);
+    addTextButton: function(msg, onclick) {
+        var b = new Button(this.x + this.pad, this.currentY, this.w - this.pad * 2, this.h, onclick);
+        b.setFont(this.font, this.fontcolor);
         b.setText(msg);
         this.buttons.push(b);
         this.buttonCount++;
         this.currentY += this.pad + this.h;
     },
 
-    addTextIconButton: function(msg,onclick,drawIcon) {
-        var b = new Button(this.x+this.pad,this.currentY,this.w-this.pad*2,this.h,onclick);
-        b.setFont(this.font,this.fontcolor);
-        b.setText(msg);
-        b.setIcon(drawIcon);
+    addHighScoresButton: function() {
+        var b = new Button(this.x + this.pad, this.currentY, this.w - this.pad * 2, this.h, this.viewHighScores.bind(this));
+        b.setFont(this.font, this.fontcolor);
+        b.setText("High Scores");
         this.buttons.push(b);
         this.buttonCount++;
         this.currentY += this.pad + this.h;
     },
 
-    addIconButton: function(drawIcon,onclick) {
-        var b = new Button(this.x+this.pad,this.currentY,this.w-this.pad*2,this.h,onclick);
-        b.setIcon(drawIcon);
-        this.buttons.push(b);
-        this.buttonCount++;
-        this.currentY += this.pad + this.h;
+    viewHighScores: function() {
+        // Here you would add the logic to display the high scores
+        var highScores = this.getHighScores();
+        alert("High Scores:\n" + highScores.join("\n"));
     },
 
-    addSpacer: function(count) {
-        if (count == undefined) {
-            count = 1;
-        }
-        this.currentY += count*(this.pad + this.h);
+    getHighScores: function() {
+        // Retrieve high scores from localStorage (or use a backend)
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        return highScores;
+    },
+
+    saveHighScore: function(name, score) {
+        // Get the current high scores from localStorage
+        var highScores = this.getHighScores();
+        
+        // Add the new score
+        highScores.push({ name: name, score: score });
+        
+        // Sort by score (descending order) and keep only the top 5
+        highScores.sort((a, b) => b.score - a.score);
+        highScores = highScores.slice(0, 5);
+        
+        // Save the new high scores back to localStorage
+        localStorage.setItem("highScores", JSON.stringify(highScores));
     },
 
     enable: function() {
         var i;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             this.buttons[i].enable();
         }
         this.enabled = true;
@@ -120,7 +121,7 @@ Menu.prototype = {
 
     disable: function() {
         var i;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             this.buttons[i].disable();
         }
         this.enabled = false;
@@ -132,21 +133,21 @@ Menu.prototype = {
 
     draw: function(ctx) {
         if (this.title) {
-            ctx.font = tileSize+"px ArcadeR";
+            ctx.font = tileSize + "px ArcadeR";
             ctx.textBaseline = "middle";
             ctx.textAlign = "center";
             ctx.fillStyle = "#FFF";
-            ctx.fillText(this.title,this.x + this.w/2, this.y+this.pad + this.h/2);
+            ctx.fillText(this.title, this.x + this.w / 2, this.y + this.pad + this.h / 2);
         }
         var i;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             this.buttons[i].draw(ctx);
         }
     },
 
     update: function() {
         var i;
-        for (i=0; i<this.buttonCount; i++) {
+        for (i = 0; i < this.buttonCount; i++) {
             this.buttons[i].update();
         }
     },
